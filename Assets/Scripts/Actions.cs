af9e198008b6a5933435b49fe0c27c34f03b4474
Frank <f.c2.de.jong@student.vu.nl>
2021-06-21 16:41:05 +0200

    added documentation

--- Assets/Scripts/Actions.cs
+++ Assets/Scripts/Actions.cs
@@ -16,31 +16,40 @@ namespace AvatarActions {
             Transform = transform;
         }
 
+        // This abstract class should implement an action you want an avatar to perform. 
         public abstract IEnumerator Trigger();
     }
 
     public class AngryGesture3 : AvatarAction {
+        // This Action triggers an animation
         public AngryGesture3(Rigidbody body, Animator animator, Transform transform) : base(body, animator, transform) {
         }
 
         public override IEnumerator Trigger() {
+            // Check to make sure we aren't triggered twice
             if (Triggered) yield break;
             Triggered = true;
-            
+
+            // In this case we only use the Animator and set a, so called trigger, this performs an animation and
+            // resets the state after the animation finishes.
             Animator.SetTrigger("AngryGesture3");
             yield return new WaitForSeconds(4f);
             yield return false;
         }
     }
-    
+
     public class AngryPoint : AvatarAction {
+        // This Action triggers an animation
         public AngryPoint(Rigidbody body, Animator animator, Transform transform) : base(body, animator, transform) {
         }
-    
+
         public override IEnumerator Trigger() {
+            // Check to make sure we aren't triggered twice
             if (Triggered) yield break;
             Triggered = true;
-            
+
+            // In this case we only use the Animator and set a, so called trigger, this performs an animation and
+            // resets the state after the animation finishes.
             yield return new WaitForSeconds(7f);
             Animator.SetTrigger("AngryPoint");
             yield return new WaitForSeconds(3f);
@@ -49,13 +58,22 @@ namespace AvatarActions {
     }
 
     public class WalkAway : AvatarAction {
+        // This class triggers a walk away action. When applied to an avatar, it turns 90 degrees and then walks away.
         public WalkAway(Rigidbody body, Animator animator, Transform transform) : base(body, animator, transform) {
         }
 
         public override IEnumerator Trigger() {
+            // Check to make sure we aren't triggered twice
             if (Triggered) yield break;
             Triggered = true;
-            
+
+            // Based on some testing the max motion values are set to a "magic" number making it look somewhat nice.
+            // As the turn is performed we assume the avatar is not moving so we need to increment the animation value
+            // to become more intense as the turn is performed. We calculate the step size by taking the max value and
+            // dividing by 22. Why 22? Again this is a bit of a magic number. We turn by 90 degrees in a for loop. To
+            // make the turn happen at a specific speed we iterate 45 times and turn the avatar by 2 degrees each time.
+            // The animation values speed up as the turn happens and slows down as it finishes. Therefore once we reach
+            // a 45 degree turn we start decreasing the animation values again. So the step size is maxValue / ~ 22.
             const float maxSideMotion = .76f;
             var currentSideMotion = 0f;
             const float stepSideMotion = maxSideMotion / 22;
@@ -64,13 +82,16 @@ namespace AvatarActions {
             var currentForwardMotion = 0f;
             const float stepForwardMotion = maxForwardMotion / 22;
 
+            // We iterate 45 times and rotate by 2 degrees each iteration.
             for (var i = 0; i < 45; i++) {
                 Body.transform.Rotate(Vector3.up * 2);
 
+                // In the first part the intensity of the animation increases.
                 if (i <= 22) {
                     currentSideMotion += stepSideMotion;
                     currentForwardMotion += stepForwardMotion;
                 } else {
+                    // and in the second part it decreases again.
                     currentSideMotion -= stepSideMotion;
                 }
 
@@ -80,9 +101,13 @@ namespace AvatarActions {
                 yield return new WaitForSeconds(0.01f);
             }
 
+            // We finishes rotating so we set our side motion to 0.
             Animator.SetFloat("PlayerSideMotion", 0f);
+
+            // We add some initial force so we start moving a bit faster.
             Body.AddForce(100f * Transform.forward);
 
+            // We loop 500 times and add some force each iteration until we reach the maximum velocity.
             for (var i = 0; i < 500; i++) {
                 Animator.SetFloat("PlayerForwardMotion", 1.14f);
                 if (Body.velocity.magnitude <= MaximumVelocity) {
@@ -92,9 +117,12 @@ namespace AvatarActions {
                 yield return new WaitForSeconds(0.01f);
             }
 
+            // We are done walking so we set our motion to 0.
+            // TODO: our force isn't 0 yet but our animation is 0. So we are gliding until our momentum is 0 again.
+            // Does this fix it? TODO
+            Body.AddForce(-Transform.forward);
             Animator.SetFloat("PlayerSideMotion", 0f);
             Animator.SetFloat("PlayerForwardMotion", 0f);
-            Body.AddForce(-Transform.forward);
 
             yield return false;
         }
--- Assets/Scripts/ModelScenario.cs
+++ Assets/Scripts/ModelScenario.cs
@@ -6,38 +6,50 @@ using FacialExpressions;
 using UnityEngine;
 
 public class ModelScenario : MonoBehaviour {
+    // We read our model here already
     private static readonly ModelData Data = new ModelData();
+
+    // The scenario has two avatars
     public GameObject avatarA;
     public GameObject avatarB;
 
+    // Each avatar has an emotion controller
     private EmotionController _avatarAEmotionController;
     private EmotionController _avatarBEmotionController;
 
+    // We read the number of rows of data.
     private readonly int _max = Data.GetColumnData("time").Count;
+
+    // We read each expression state that is relevant to the virtualization.
     private readonly List<float> _x11Break = Data.GetColumnData("X11 - Break");
     private readonly List<float> _x12Anger = Data.GetColumnData("X12 - anger");
     private readonly List<float> _x13Threaten = Data.GetColumnData("X13 - threaten");
     private readonly List<float> _x21Gazeaway = Data.GetColumnData("X21 - gazeaway");
     private readonly List<float> _x22Walkaway = Data.GetColumnData("X22 - walkaway ");
 
+    // An avatar has an walk away action
     private AvatarAction _walkAway;
 
     private void ModelDataToAvatar(float x11, float x12, float x13, float x21, float x22) {
-        // Debug.Log(x11 + " " + x12 + " " + x13 + " " + x21 + " " + x22);
+        // Once our walk away state reaches the threshold of 0.5 we trigger walking away. 
         if (x22 >= 0.5) {
             if (!_walkAway.Triggered) StartCoroutine(_walkAway.Trigger());
         }
 
+        // Each row we read a new angry state so we have to set it each time.
         StartCoroutine(_avatarAEmotionController.SetEmotion("angry", x12 * 100));
+
+        // TODO: x11, x13, x21
     }
 
     private IEnumerator ApplyDataToAvatars(float yieldTime) {
         // Calling this function twice will result in some weird behavior. Maybe there should be a check to prevent
         // this from happening accidentally? TODO
 
+        // We loop through each row of the data and get each value in that row. We call a function which will apply
+        // the data to the avatars.
+        // i = 45 (we skip the first 45 rows of all zeros)
         for (var i = 45; i < _max; i++) {
-            // Here we get all values in one row and call a function which converts the data to some action of the 
-            // avatar.
             ModelDataToAvatar(_x11Break[i], _x12Anger[i], _x13Threaten[i], _x21Gazeaway[i], _x22Walkaway[i]);
 
             // We wait for yieldTime. After the execution continues in this loop having kept all the values.
@@ -49,6 +61,9 @@ public class ModelScenario : MonoBehaviour {
     }
 
     private void Start() {
+        // IMPORTANT: avatarA and avatarB need to be set inside of Unity.
+
+        // Read the emotion controllers
         _avatarAEmotionController = avatarA.GetComponent<PlayerEmotionController>().Controller;
         _avatarBEmotionController = avatarB.GetComponent<PlayerEmotionController>().Controller;
 
@@ -64,15 +79,18 @@ public class ModelScenario : MonoBehaviour {
                                 "Without this attached we cannot set the emotions");
         }
 
+        // Read time data.
         var time = Data.GetColumnData("time");
         if (time == null) {
             Debug.LogWarning("We have not read time data!");
             return;
         }
 
+        // We initialize the walk away action with its Body, Animator and Transform objects
         _walkAway = new WalkAway(avatarB.GetComponent<Rigidbody>(), avatarB.GetComponent<Animator>(),
             avatarB.GetComponent<Transform>());
 
+        // Test action: trigger a simple animation
         StartCoroutine(new AngryPoint(avatarA.GetComponent<Rigidbody>(), avatarA.GetComponent<Animator>(),
             avatarA.GetComponent<Transform>()).Trigger());
 
